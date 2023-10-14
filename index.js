e0df2251efb5f20d1622177ce2144442341ab04d
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-10-14 22:50:44 +0300

    array method lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+
+  function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+
+  function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+
+  
+  function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+     
+
+  function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+  
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
